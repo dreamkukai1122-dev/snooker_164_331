609214bf875fc2bcb35bd18b8ab56ccb7d9a623d
dreamkukai1122-dev <dreamkukai1122@gmail.com>
2026-07-31 23:38:10 +0700

    cm5

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -20,6 +20,7 @@ public class Ball : MonoBehaviour
 
     [SerializeField]
     private BallColor color;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
